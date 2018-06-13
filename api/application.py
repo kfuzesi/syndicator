852fe48a7a2edf6ca1bfed0c0079e09a4ebcd80f
Katharine Fuzesi <Katharine.Fuzesi@ibm.com>
2018-06-12 23:35:06 -0400

    Cannot get flask to deploy to aws

--- api/application.py
+++ api/application.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from tables import *
-from resources import *
+#from resources import *
 
 class HelloWorld(Resource):
     def get(self):
@@ -12,18 +12,18 @@ class HelloWorld(Resource):
 
 def setup_resources(api):
     api.add_resource(HelloWorld, '/', '/hello')
-    api.add_resource(ProductList, '/products')
-    api.add_resource(Product, '/products/<id>', endpoint='product')
-    api.add_resource(EventList, '/events')
-    api.add_resource(Event, '/events/<id>', endpoint='event')
+#    api.add_resource(ProductList, '/products')
+#    api.add_resource(Product, '/products/<id>', endpoint='product')
+#    api.add_resource(EventList, '/events')
+#    api.add_resource(Event, '/events/<id>', endpoint='event')
 
 #def create_app():
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI']='sqlite:///../syndicator_repo.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
-db.init_app(application)
-db.create_all(app=application)
+#db.init_app(application)
+#db.create_all(app=application)
 
 api = Api(application)
 setup_resources(api)
