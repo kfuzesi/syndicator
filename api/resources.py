88f213185770879f7d3dfcd24c224b52904c8572
Katharine Fuzesi <Katharine.Fuzesi@ibm.com>
2018-06-12 22:41:45 -0400

    Added files to deploy flask to aws.

---
+++ api/resources.py
@@ -0,0 +1,164 @@
+import uuid
+
+from flask import jsonify, make_response, request, json
+from flask_restful import Resource
+from tables import *
+
+def make_error(message, code):
+    error = {
+        'error' : {
+            'message': message,
+            'code': code
+        }
+    }
+    return make_response(jsonify(error), code)
+
+class ProductList(Resource):
+
+    def get(self):
+        products = ProductDB.query.all()
+        return jsonify([s.to_dict() for s in products])
+
+    def post(self):
+        data = request.get_json()
+
+        if not data:
+            return make_error("Need json data", 400)
+
+        if not data.has_key('name'):
+            return make_error("Need parameter 'name'", 400)
+
+        if not data.has_key('price'):
+            return make_error("Need parameter 'price'", 400)
+
+        # optional inputs
+        description = None
+        if data.has_key('description'):
+            description = data['description']
+
+        organizer_id = None
+        if data.has_key('organizer_id'):
+            organizer_id = data['organizer_id']
+
+        id = str(uuid.uuid4())
+
+        # save to database
+        product = ProductDB(id=id, name=data['name'], price=data['price'],
+                            description=description, organizer_id=organizer_id)
+        db.session.add(product)
+        db.session.commit()
+
+        response = {
+            'message': 'Post received',
+            'id': id
+        }
+        return make_response(jsonify(response), 200)
+
+class Product(Resource):
+
+    def get(self, id):
+        product = ProductDB.query.get(id)
+
+        if product:
+            return make_response(jsonify(product.to_dict()), 200)
+        else:
+            return make_error("Product with id '" + str(id) + "' does not exist", 400)
+
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        product = ProductDB.query.get(id)
+
+        if product:
+            db.session.delete(product)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Product was deleted'}), 200)
+        else:
+            return make_error("Product with id '" + str(id) + "' does not exist", 400)
+
+class EventList(Resource):
+
+    def get(self):
+        events = EventDB.query.all()
+        return jsonify([s.to_dict() for s in events])
+
+    def post(self):
+        data = request.get_json()
+
+        # check for required parameters
+        if not data:
+            return make_error("Need json data", 400)
+
+        if not data.has_key('name'):
+            return make_error("Need parameter 'name'", 400)
+
+        if not data.has_key('price'):
+            return make_error("Need parameter 'price'", 400)
+
+        if not data.has_key('start_time'):
+            return make_error("Need parameter 'start_time'", 400)
+
+        if not data.has_key('start_timezone'):
+            return make_error("Need parameter 'start_timezone'", 400)
+
+        if not data.has_key('end_time'):
+            return make_error("Need parameter 'end_time'", 400)
+
+        if not data.has_key('end_timezone'):
+            return make_error("Need parameter 'end_timezone'", 400)
+
+        # optional inputs
+        description = None
+        if data.has_key('description'):
+            description = data['description']
+
+        organizer_id = None
+        if data.has_key('organizer_id'):
+            organizer_id = data['organizer_id']
+
+        # other inputs
+        id = str(uuid.uuid4())
+        start_time = datetime.strptime(data['start_time'], '%Y-%m-%dT%I:%M:%S')
+        end_time = datetime.strptime(data['end_time'], '%Y-%m-%dT%I:%M:%S')
+        print start_time
+        print end_time
+
+        # save to database
+        event = EventDB(id=id, name=data['name'], price=data['price'],
+                            description=description, organizer_id=organizer_id,
+                            start_time=start_time, start_timezone=data['start_timezone'],
+                            end_time=end_time, end_timezone=data['end_timezone'])
+        db.session.add(event)
+        db.session.commit()
+
+        response = {
+            'message': 'Post received',
+            'id': id
+        }
+        return make_response(jsonify(response), 200)
+
+class Event(Resource):
+
+    def get(self, id):
+        event = EventDB.query.get(id)
+
+        if event:
+            return make_response(jsonify(event.to_dict()), 200)
+        else:
+            return make_error("Event with id '" + str(id) + "' does not exist", 400)
+
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        event = ProductDB.query.get(id)
+
+        if event:
+            db.session.delete(event)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Event was deleted'}), 200)
+        else:
+            return make_error("Event with id '" + str(id) + "' does not exist", 400)
---
+++ api/tables.py
@@ -0,0 +1,36 @@
+
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Base table with to_dict function that all tables will inherit
+class BaseDB(db.Model):
+    __abstract__ = True
+
+    def to_dict(self):
+        db_dict = {}
+        for key in self.__table__.columns._data.keys():
+            db_dict[key] = getattr(self, key)
+        return db_dict
+
+class ProductDB(BaseDB):
+    __tablename__ = 'product'
+    id = db.Column(db.String, primary_key=True, unique=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(500), nullable=True)
+    organizer_id = db.Column(db.String(50), nullable=True)
+    price = db.Column(db.Float)
+
+class EventDB(BaseDB):
+    __tablename__ = 'event'
+    id = db.Column(db.String, primary_key=True, unique=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(500), nullable=True)
+    organizer_id = db.Column(db.String(50), nullable=True)
+    price = db.Column(db.Float)
+    start_time = db.Column(db.DateTime, default=datetime.utcnow)
+    start_timezone = db.Column(db.String(50))
+    end_time = db.Column(db.DateTime, default=datetime.utcnow)
+    end_timezone = db.Column(db.String(50))
